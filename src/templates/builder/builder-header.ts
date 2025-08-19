d1880006b5c6ca6887d92fcf938801ddf5056ce9
Gilang Irfansyah <oadacode@gmail.com>
2025-08-19 18:05:36 +0700

    feat: implement header builder for PDF documents

---
+++ src/templates/builder/builder-header.ts
@@ -0,0 +1,55 @@
+import { logo } from "../../data/logo.js";
+import type { InvoiceData } from "../../types/opd.js";
+import { buildAdmissionInfo } from "./builder-admission.js";
+
+export const buildHeader = (data: InvoiceData) => {
+  return {
+    stack: [
+      {
+        columns: [
+          {
+            stack: [
+              {
+                image: logo,
+                width: 120,
+                height: 30,
+                margin: [0, 10, 0, 5],
+              },
+
+              {
+                text: data.company_details.company_name,
+                style: 'headerSubtitle',
+              },
+              {
+                text: data.company_details.company_address,
+                style: 'headerAddress',
+              },
+              {
+                text: data.company_details.company_contact_no,
+                style: 'headerAddress',
+              },
+            ],
+            width: '*',
+            alignment: 'center',
+          },
+        ],
+      },
+      {
+        margin: [30, 10, 30, 0],
+        columns: [
+          {
+            width: '50%',
+            alignment: 'right',
+            stack: [{ text: 'INVOICE', bold: true, fontSize: 10 }],
+          },
+          {
+            width: '50%',
+            alignment: 'right',
+            stack: [{ qr: 'OIV2508150003', fit: 30, bold: true }],
+          },
+        ],
+      },
+      buildAdmissionInfo(data.admission_details),
+    ],
+  };
+};
